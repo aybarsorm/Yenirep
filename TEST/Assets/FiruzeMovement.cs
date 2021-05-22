64a2613ee485d211e011b3bf591e1ff83f0a1b54
BatuhanOzdemir-dev <batuhan22.ozdemir@gmail.com>
2021-05-22 22:17:18 +0300

    Firuze Hanım'ı Ekledim
    Firuze Hanım'ın modelini oyuna ekledim ve biraz animasyon falan yaptım. Sıkıntı şu ki, animasyonlar kendiliğinden hareket ediyor.
    TODO: Animasyonları stationary olanıyla değiştireceğim.

---
+++ TEST/Assets/FiruzeMovement.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class FiruzeMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public CinemachineFreeLook cine;
+    private float gravity = 9.81f;
+    public float turnSmoothTime = 0.1f;
+    private float directionY;
+    private float turnSmoothVelocity;
+    float changeQ = 90.0f;
+    float changeE = -90.0f;
+    public Animator anim;
+
+    private float newAngle;
+    public float camTurnSpeed = 2;
+    private void Awake()
+    {
+        newAngle = cine.m_XAxis.Value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeCameraAngle();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        Vector3 toGround = new Vector3(0, 0, 0).normalized;
+        if (controller.isGrounded)
+        {
+            directionY = -1.5f;
+            //Debug.Log("NO");
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle + 180f, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * (speed * Time.deltaTime));
+
+                anim.SetBool("Walk", true);
+            }
+            else
+            {
+                anim.ResetTrigger("Walk");
+            }
+        }
+        else
+        {
+            //Debug.Log("YES");
+            directionY -= gravity * Time.deltaTime;
+            toGround.y = directionY;
+            controller.Move(toGround * (speed * Time.deltaTime));
+        }
+
+
+    }
+
+    void changeCameraAngle()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            newAngle += changeQ;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            newAngle += changeE;
+        }
+
+        cine.m_XAxis.Value = Mathf.Lerp(cine.m_XAxis.Value, newAngle, Time.fixedDeltaTime * camTurnSpeed);
+    }
+}
